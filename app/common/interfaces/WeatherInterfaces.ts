3e7c5e357d101a075674527d0ce301063b0d489d
Howard Pinsley <hpinsley@gmail.com>
2015-12-14 08:26:09 -0500

    Update the weather service to publish lnglat coords

--- app/common/interfaces/WeatherInterfaces.ts
+++ app/common/interfaces/WeatherInterfaces.ts
@@ -1,5 +1,6 @@
 export interface IWeatherUpdate {
 	city: string;
+	lnglat: [number, number];
 	time: Date;
 	tempFarenheit: number;
 }
--- app/server/workers/WeatherService.ts
+++ app/server/workers/WeatherService.ts
@@ -1,17 +1,30 @@
 import {IWeatherUpdate} from '../../common/interfaces/WeatherInterfaces';
 import * as Rx from '@reactivex/rxjs';
 
+interface ILocation {
+	name: string;
+	lnglat: [number, number]
+};
+
 export class WeatherService {
 
 	sleepTime = 2000;
 	weatherPub: Rx.Subject<IWeatherUpdate>;
 	timer: any;
-	cities:string[];
+	cities:ILocation[];
 
 	constructor() {
-		this.cities = ['New York', 'Los Angeles', 'Chicago', 'Miami', 'Dallas', 'Washington'];
+		this.cities = [
+			{ name: 'New York', lnglat: [ -71.926802380749692, 41.290122059153795 ] },
+			{ name: 'Chicago', lnglat: [ -87.532776, 39.971077 ] },
+			{ name: 'Los Angeles', lnglat: [ -122.430957929966013, 37.8722420698438 ] },
+			{ name: 'Miami', lnglat: [ -85.114268, 29.688658 ] },
+			{ name: 'Washington DC', lnglat: [ -76.029463, 37.953775 ] },
+			{ name: 'Dallas', lnglat: [ -97.132473080391691, 27.8977671032813 ] },
+			{ name: 'Las Vegas', lnglat: [ -114.050167, 36.624978 ] }
+			];
 		this.weatherPub = new Rx.Subject<IWeatherUpdate>();
-		//this.start();
+		this.start();
 	}
 
 	getWeatherUpdatePub() : Rx.Subject<IWeatherUpdate> {
@@ -34,7 +47,8 @@ export class WeatherService {
 		let index:number = Math.floor(Math.random() * this.cities.length);
 
 		let update:IWeatherUpdate = {
-			city: this.cities[index],
+			city: this.cities[index].name,
+			lnglat: this.cities[index].lnglat,
 			time: new Date(),
 			tempFarenheit: Math.floor(Math.random() * 80) + 20
 		};
