f2a75dd4441094d1217cedacf1571a4cf934591d
Howard Pinsley <hpinsley@gmail.com>
2015-12-15 08:32:58 -0500

    Update the message broker to publish accidents

--- app/services/MessageBroker.ts
+++ app/services/MessageBroker.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from 'angular2/angular2';
 import {IWeatherUpdate} from '../common/interfaces/WeatherInterfaces';
+import {IAccident} from '../common/interfaces/TrafficInterfaces';
 
 @Injectable()
 export class MessageBroker {
@@ -7,6 +8,7 @@ export class MessageBroker {
 	io: SocketIOClientStatic;
 	socket: SocketIOClient.Socket;
 	weatherUpdates: EventEmitter<IWeatherUpdate>;
+	accidentUpdates: EventEmitter<IAccident>;
 
 	constructor() {
 		// Connect to the server
@@ -15,10 +17,16 @@ export class MessageBroker {
 
 		// Create publications
 		this.weatherUpdates = new EventEmitter<IWeatherUpdate>();
+		this.accidentUpdates = new EventEmitter<IAccident>();
 
 		// Set up publication updating
 		this.socket.on('weatherUpdate', (update:IWeatherUpdate) => {
 			this.weatherUpdates.next(update);
+		});
+
+		this.socket.on('accident', (accident:IAccident) => {
+			console.log(`Accident involving ${accident.vehiclesInvolved} vehicles in ${accident.state} at ${accident.time}.`);
+			this.accidentUpdates.next(accident);
 		});
 	}
 
