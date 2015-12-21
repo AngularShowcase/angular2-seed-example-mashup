a24e908a6e12d73177de4d98e95807a99f72cce9
Howard Pinsley <hpinsley@gmail.com>
2015-12-20 21:28:01 -0500

    Main page loads with default server

--- tools/config.ts
+++ tools/config.ts
@@ -47,7 +47,9 @@ export const NPM_DEPENDENCIES = [
   { src: 'angular2/bundles/router.js', inject: 'libs', dest: LIB_DEST }, // use router.min.js with alpha47
   { src: 'angular2/bundles/http.min.js', inject: 'libs', dest: LIB_DEST },
 
-  { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, dest: CSS_DEST }
+  { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, dest: CSS_DEST },
+
+  { src: 'socket.io-client/socket.io.js', inject: 'libs', dest: LIB_DEST }
 ];
 
 // Declare local files that needs to be injected
