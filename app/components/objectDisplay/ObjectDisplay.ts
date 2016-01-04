a593779d94a7233e711aaae81f496ebb89d10132
Howard Pinsley <hpinsley@gmail.com>
2016-01-03 23:23:30 -0500

    ObjectDisplay now working the way it had been before.

--- app/components/objectDisplay/ObjectDisplay.ts
+++ app/components/objectDisplay/ObjectDisplay.ts
@@ -5,7 +5,7 @@ import {Parser} from '../../services/Parser';
 @Component({
     selector: 'object-display',
     providers: [Parser],
-    inputs: ['displayObject:display-object','niceLabels:nice-labels','collapsed'],
+    inputs: ['displayObject:display-object', 'niceLabels:nice-labels', 'collapsed'],
     outputs: ['selectobject:selectobject'],
     templateUrl: './components/objectDisplay/ObjectDisplay.html',
     styleUrls: ['./components/objectDisplay/ObjectDisplay.css'],
@@ -13,94 +13,107 @@ import {Parser} from '../../services/Parser';
 })
 export class ObjectDisplay {
 
-       _displayObject: any;
-       niceLabels: boolean;
-       collapsed:boolean;
-       selectobject: EventEmitter<any>;
-       parser:Parser;
-       objectProperties: string[] = [];
+    _displayObject: any;
+    _collapsed: boolean;
+    niceLabels: boolean;
+    selectobject: EventEmitter<any>;
+    parser: Parser;
+    objectProperties: string[] = [];
 
-       constructor(parser:Parser) {
-           console.log('In ObjectDisplay constructor');
-           this.parser = parser;
-           this.niceLabels = true;
-           this.collapsed = false;
-           //this.displayObject = {};
-           this.selectobject = new EventEmitter<any>();
-       }
+    constructor(parser: Parser) {
+        console.log('In ObjectDisplay constructor');
+        this.parser = parser;
+        this.niceLabels = true;
+        this.collapsed = false;
+        //this.displayObject = {};
+        this.selectobject = new EventEmitter<any>();
+    }
 
-       get displayObject() {
-           return this._displayObject;
-       }
+    get collapsed() : boolean {
+        return this._collapsed;
+    }
 
-       set displayObject(val) {
-           console.log('Setting display object to: ', val);
-           this._displayObject = val;
-           this.objectProperties = this.getObjectProperties(this.collapsed);
-       }
+    set collapsed(val:boolean) {
+        console.log(`Collapsed being set to ${val}`);
+        this._collapsed = val;
+        this.objectProperties = this.getObjectProperties();
+    }
 
-       getType():string {
-           if (Array.isArray(this._displayObject)) {
-               return 'array';
-           }
-           return typeof(this._displayObject);
-       }
+    get displayObject() {
+        return this._displayObject;
+    }
 
-       getTheType(val:any) {
-           return typeof(val);
-       }
+    set displayObject(val) {
+        console.log('Setting display object to: ', val);
+        this._displayObject = val;
+        this.objectProperties = this.getObjectProperties();
+    }
 
-       isObject():boolean {
-           return this.getType() === 'object';
-       }
+    getType(): string {
+        if (Array.isArray(this._displayObject)) {
+            return 'array';
+        }
+        return typeof (this._displayObject);
+    }
 
-       isArray():boolean {
-           return this.getType() === 'array';
-       }
+    getTheType(val: any) {
+        return typeof (val);
+    }
 
-       // If we are getting only the 'first' property, the object being displayed is collapsed.  We
-       // want to show at least one property and an expander.  We favor any property with the word
-       // name at the beginning.  If none is found, just return the first property.
+    isObject(): boolean {
+        return this.getType() === 'object';
+    }
 
-       private getObjectProperties(firstOnly?:boolean):string[] {
-           var props:string[] = [];
+    isArray(): boolean {
+        return this.getType() === 'array';
+    }
 
-           if (!this.isObject()) {
-               return props;
-           }
+    // If we are getting only the 'first' property, the object being displayed is collapsed.  We
+    // want to show at least one property and an expander.  We favor any property with the word
+    // name at the beginning.  If none is found, just return the first property.
 
-           for (var prop in this.displayObject) {
-               if (firstOnly && /.*name.*/.test(prop.toLowerCase())) {
-                   return [prop];
-               }
-               props.push(prop);
-           }
+    toggleCollapsed() {
+        this.collapsed = !this.collapsed;
+    }
 
-           var result = (firstOnly) ? props.slice(0,1) : props;
-           console.log(`getObjectProperties: ${result}`);
-           return result;
-       }
+    objectClick(ev) {
+        //console.log('click on', ev, this.displayObject);
+        this.selectobject.next(this);
+    }
 
-       toggleCollapsed() {
-           this.collapsed = !this.collapsed;
-       }
+    objectSelected($event) {
+        //console.log('Propagating', $event);
+        this.selectobject.next($event);
+    }
 
-       objectClick(ev) {
-            //console.log('click on', ev, this.displayObject);
-            this.selectobject.next(this);
-       }
+    propNameToLabel(propName: string): string {
 
-       objectSelected($event) {
-           //console.log('Propagating', $event);
-           this.selectobject.next($event);
-       }
+        if (!this.niceLabels) {
+            return propName;
+        }
 
-       propNameToLabel(propName:string):string {
+        return this.parser.propertyNameToLabel(propName);
+    }
 
-           if (!this.niceLabels) {
-               return propName;
-           }
+    private getObjectProperties(): string[] {
 
-           return this.parser.propertyNameToLabel(propName);
-       }
+        let firstOnly: boolean = this.collapsed;
+
+        var props: string[] = [];
+
+        if (!this.isObject()) {
+            return props;
+        }
+
+        for (var prop in this.displayObject) {
+            if (firstOnly && /.*name.*/.test(prop.toLowerCase())) {
+                return [prop];
+            }
+            props.push(prop);
+        }
+
+        var result = (firstOnly) ? props.slice(0, 1) : props;
+        console.log(`getObjectProperties: ${result}`);
+        return result;
+    }
 }
