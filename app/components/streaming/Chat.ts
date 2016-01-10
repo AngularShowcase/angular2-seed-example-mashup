e0ee85fa0c68619f320223adf1dab7a14e42278d
Howard Pinsley <hpinsley@gmail.com>
2016-01-10 00:03:52 -0500

    Start of change of chat ui

--- app/components/streaming/Chat.ts
+++ app/components/streaming/Chat.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {MessageBroker} from '../../services/MessageBroker';
 import {Authentication} from '../../services/Authentication';
 import {IRegisteredUser} from '../../../common/interfaces/RegistrationInterfaces';
@@ -7,10 +8,10 @@ import {IChatMessage} from '../../../common/interfaces/ChatInterfaces';
 
 @Component({
     selector: 'chat',
-    templateUrl: './components/streaming/Chat.html',
+    templateUrl: './components/streaming/Chat-Material.html',
     styleUrls: ['./components/streaming/Chat.css'],
     pipes: [],
-    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
+    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES, MATERIAL_DIRECTIVES]
 })
 export class Chat {
 
