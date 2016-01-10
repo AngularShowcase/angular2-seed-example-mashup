52a3d2c20774143dfd8c5240a55239b2e849ab89
Howard Pinsley <hpinsley@gmail.com>
2016-01-09 20:27:46 -0500

    Cleaned up the login component

--- app/components/login/Login.ts
+++ app/components/login/Login.ts
@@ -3,69 +3,40 @@ import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from 'ng2-material/all';
 import {Router} from 'angular2/router';
 import {Authentication} from '../../services/Authentication';
-import {HpFlyText} from '../../directives/HpFlyText';
 
 @Component({
     selector: 'login',
     templateUrl: './components/login/login-material.html',
-    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES, HpFlyText, MATERIAL_DIRECTIVES]
+    directives: [FORM_DIRECTIVES, CORE_DIRECTIVES, MATERIAL_DIRECTIVES]
 })
 export class Login {
+
+    username:string;
+    password:string;
 
     constructor(public auth:Authentication, public router:Router) {
     }
 
-    loggedIn: boolean = false;
-
-    model:any = {
-        username: '',
-        password: '',
-        flyText1: '',
-        errors: ''
-    };
-
     login() {
         console.log('Last route in login() is: ', this.auth.getLastRoute());
 
-        if (!this.haveAllFields()) {
-            this.model.errors = 'Please fill out the form.';
-            return;
-        }
-
-        this.auth.login(this.model.username, this.model.password)
+        this.auth.login(this.username, this.password)
             .subscribe(loginResult => {
                 if (!loginResult.succeeded) {
                     alert('Login failed');
                     return;
                 }
 
-                this.model.errors = '';
-                this.loggedIn = true;
-                this.model.flyText1 = this.auth.user.username;
+                // Navigate to the last url now that we are logged in
+                var lastUrl = this.auth.getLastRoute();
+
+                if (lastUrl) {
+                    var redirect = '/' + lastUrl;
+                    console.log(`Login is redirecting to ${redirect}`);
+                    this.router.navigateByUrl(redirect);
+                } else {
+                    this.router.navigateByUrl('/');
+                }
             });
-    }
-
-    onAnimationComplete() {
-        if (this.auth.isLoggedIn()) {
-            // Navigate to the last url now that we are logged in
-            var lastUrl = this.auth.getLastRoute();
-
-            if (lastUrl) {
-                var redirect = '/' + lastUrl;
-                console.log(`Login is redirecting to ${redirect}`);
-                this.router.navigateByUrl(redirect);
-            } else {
-                this.router.navigateByUrl('/');
-            }
-        }
-    }
-
-    haveAllFields() {
-        return (this.model.username &&
-                this.model.password);
-    }
-
-    buttonClass() : string {
-        return this.haveAllFields() ? 'btn-info' : 'btn-danger';
     }
 }
