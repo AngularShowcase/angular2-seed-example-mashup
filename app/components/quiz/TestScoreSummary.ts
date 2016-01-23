d194ccd654e26dc79177e129716fe2ab92ac5a61
Howard Pinsley <hpinsley@gmail.com>
2016-01-22 19:39:17 -0500

    Remove unneeded init code

--- app/components/quiz/TestScoreSummary.ts
+++ app/components/quiz/TestScoreSummary.ts
@@ -12,21 +12,5 @@ import {ITest} from '../../../common/interfaces/QuizInterfaces';
 })
 export class TestScoreSummary {
 
-    test:ITest = {
-        testId: 0,
-        quizId: 0,
-        user: '',
-        questionCount: 0,
-        dateTaken: new Date(),
-        completed: false,
-        answers: [],
-        sectionResults: [],
-        testResult: {
-            sectionName: 'total',
-            questionCount: 0,
-            correctCount: 0,
-            incorrectCount: 0,
-            score: 0
-        }
-    };
+    test:ITest;
 }
