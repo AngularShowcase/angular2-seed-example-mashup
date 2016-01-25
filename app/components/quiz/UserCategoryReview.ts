f4ac8dda123f4ea0edbe8ba5fd42aad50a7d4e20
Howard Pinsley <hpinsley@gmail.com>
2016-01-25 09:18:59 -0500

    Start collecting user category stats

--- app/components/quiz/UserCategoryReview.ts
+++ app/components/quiz/UserCategoryReview.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter} from 'angular2/core';
 import {CORE_DIRECTIVES} from 'angular2/common';
 import {QuizServices} from '../../services/QuizServices';
-import {ITest} from '../../../common/interfaces/QuizInterfaces';
+import {ITest, ISectionResult} from '../../../common/interfaces/QuizInterfaces';
 
 @Component({
     selector: 'user-category-review',
@@ -14,6 +14,8 @@ import {ITest} from '../../../common/interfaces/QuizInterfaces';
 export class UserCategoryReview {
 
     username:string = '';
+    summarizedResults:ISectionResult[] = [];
+
     constructor(public quizServices:QuizServices) {
     }
 
@@ -21,6 +23,26 @@ export class UserCategoryReview {
 
         console.log(`UserCategoryReview invoked with username ${this.username}.`);
         if (this.username) {
+            // var x = this.quizServices.getUserTests(this.username)
+            //     .map(tests => {
+            //         let catResults:ISectionResult[] = [];
+
+            //         for (let t of tests) {
+            //             for (let s of t.sectionResults) {
+            //                 catResults.push(s);
+            //             }
+            //         }
+            //         return catResults;
+            //     });
+
+            var categoryResults
+                = this.quizServices.getUserTests(this.username)
+                    .map(tests => <ISectionResult[]> _.flatten(tests.map(test => test.sectionResults)));
+
+            categoryResults.subscribe(catResults => {
+                let dict = _.groupBy(catResults, "sectionName");
+                console.log(dict);
+            });
         }
     }
 }
